8620b68faa53233e242f0fbae96225c6583e44ae
Adrian Zanga <adrian.zanga@icloud.com>
2020-01-15 13:54:21 +0100

    create server folder

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,5 +1,6 @@
 const monk = require('monk');
+
 const connectionString = process.env.MONGODB_URI || 'localhost/ricochet-robots';
 const db = monk(connectionString);
- 
+
 module.exports = db;
--- server/index.js
+++ server/index.js
@@ -2,40 +2,41 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
- 
+
 const games = require('./db/games');
 
-
-
 const app = express();
- 
+
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(bodyParser.json());
- 
+
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Behold The MEVN Stack!'
-    });
+  res.json({
+    message: 'Behold The MEVN Stack!',
+  });
 });
 
 app.get('/games', (req, res) => {
-    games.getAll().then((games) => {
-        res.json(games);
-    });
+  games.getAll().then((g) => {
+    res.json(g);
+  });
 });
 
 app.post('/games', (req, res) => {
-    console.log(req.body);
-    games.create(req.body).then((game) => {
-        res.json(game);
-    }).catch((error) => {
-        res.status(500);
-        res.json(error);
+  console.log(req.body);
+  games
+    .create(req.body)
+    .then((g) => {
+      res.json(g);
+    })
+    .catch((error) => {
+      res.status(500);
+      res.json(error);
     });
 });
- 
+
 const port = process.env.PORT || 4001;
 app.listen(port, () => {
-    console.log(`listening on ${port}`);
+  console.log(`listening on ${port}`);
 });
